180e8eddc6fba63bd99a972099096bbb854a8010
MishaITSTEP <mishakravchuk2004@gmail.com>
2025-01-18 10:17:24 +0200

    Format code

--- OLX.Frontend/src/pages/default/home/index.tsx
+++ OLX.Frontend/src/pages/default/home/index.tsx
@@ -1,8 +1,8 @@
-const HomePage:React.FC = () => {
-  
-    return (
-      <div className=" flex-1 flex justify-center items-center">Home page</div>
-    );
-  };
-  
-  export default HomePage;
+const HomePage: React.FC = () => {
+
+  return (
+    <div className=" flex-1 flex justify-center items-center">Home page</div>
+  );
+};
+
+export default HomePage;
